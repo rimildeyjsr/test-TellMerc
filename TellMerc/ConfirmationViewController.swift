ffc101d1a672ba23903c81c7da2beba385d31018
Rimil Dey <rimildeyjsr@gmail.com>
2018-04-09 00:18:45 +0530

    some stuff updated

--- TellMerc/ConfirmationViewController.swift
+++ TellMerc/ConfirmationViewController.swift
@@ -13,11 +13,20 @@ class ConfirmationViewController: UIViewController {
     // MARK: - outlets
    
     @IBOutlet weak var confirmMessage: UILabel!
+    @IBOutlet weak var okButton: UIButton!
     
     // MARK: - view did load
     override func viewDidLoad() {
         super.viewDidLoad()
         confirmMessage.text! = "You have successfully enrolled for the Test Drive event in Chennai. You will get the details shortly."
+        setupButton()
+    }
+    
+    func setupButton () {
+        okButton.backgroundColor = .clear
+        okButton.layer.cornerRadius = 25
+        okButton.layer.borderWidth = 2
+        okButton.layer.borderColor = UIColor.black.cgColor
     }
     
 }
